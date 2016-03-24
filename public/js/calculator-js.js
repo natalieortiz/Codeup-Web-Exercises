f3d7cbdf4d5121eba1a8abe529880f248d42c4e8
Natalie Ortiz <natalieortiz@Natalies-MacBook-Air.local>
2016-03-24 15:23:26 -0500

    Added javascript for negative number, decimal, and percentage.

--- public/js/calculator-js.js
+++ public/js/calculator-js.js
@@ -1,16 +1,10 @@
 (function (){
-
-    //click a button and get it in an input field
-    var firstOperand;
-    var operator;
-    var secondOperand;
-    var output;
+    
+    var decimal1 = true;
+    var decimal2 = true; 
     var inputState = true; 
 
-    //global variable to track the state of our input.
-    //if value is false go into left hand side.  when we hit an operator, we change the value to true.
-    //then send all inputs to the right side.  
-        
+    //Clicking on numbers.      
     var numbers = document.getElementsByClassName('num-btn')
     for (var i=0; i < numbers.length; i++) { 
         numbers[i].addEventListener('click', function() {
@@ -24,22 +18,64 @@
         });
     }
 
+    //Add a decimal to a number.  
+    var decimal = document.getElementById('decimal');
+    decimal.addEventListener('click', function(){ 
+        if (inputState == true && decimal1 == true) {
+            document.getElementById('left_operand').value += this.getAttribute("data-number");
+            decimal1 = false;
+        } else if (inputState == false && decimal2 == true) {
+            document.getElementById('right_operand').value += this.getAttribute("data-number");
+            decimal2 = false;
+        }
+        
+    });
+
+    //Change the sign of a number.
+    var sign = document.getElementById('sign');
+    sign.addEventListener('click', function(){
+        var negativeNum; 
+        if (inputState == true) {
+            negativeNum = parseFloat(document.getElementById('left_operand').value) * -1;
+            document.getElementById('left_operand').value = negativeNum;
+        } else if (inputState == false) {
+            negativeNum = parseFloat(document.getElementById('right_operand').value) * -1;
+            document.getElementById('right_operand').value = negativeNum;
+        }
+        
+    });
+
+    //Get percentage of a number.
+    var percentNum = document.getElementById('percentage');
+    percentNum.addEventListener('click', function(){
+        var percent; 
+        if (inputState == true) {
+            percent = parseFloat(document.getElementById('left_operand').value) * .001;
+            document.getElementById('left_operand').value = percent;
+        }
+        
+    });
+
+
     //get operator in the operator input
     var operators = document.getElementsByClassName('operator')
     for (var i=0; i < operators.length; i++){
         operators[i].addEventListener('click', function() {
-            operator = this.getAttribute("data-operator");
-            console.log(operator);
-            document.getElementById('operator').value = operator;
+            document.getElementById('operator').value = this.getAttribute("data-operator");
             inputState = false; 
         })
     }
 
 
     var equals = document.getElementById('equals');
-    equals.addEventListener("click", function(){ 
+    equals.addEventListener("click", function(){
+        var firstOperand;
+        var operator;
+        var secondOperand;
+        var output;
         firstOperand = document.getElementById('left_operand').value;
         secondOperand = document.getElementById('right_operand').value;
+        operator = document.getElementById('operator').value;
         switch (operator){
             case "+":
                 output = firstOperand + secondOperand;
@@ -54,12 +90,14 @@
                 output = firstOperand / secondOperand;
                 break;
         }
+        inputState == true; 
         document.getElementById('operator').value = '';
         document.getElementById('right_operand').value = '';
-        document.getElementById('left_operand').value = output;
+        document.getElementById('left_operand').value = output.toFixed(3);
 
     });
 
+    //CLEAR button
     var clear = document.getElementById('clear');
     clear.addEventListener('click', function(){
         inputState = true;
