a46e0b2efdc8f3652abda50e313d28cffc6b4b6f
Natalie Ortiz <natalieortiz@Natalies-MacBook-Air.local>
2016-03-30 15:07:23 -0500

    Using jquery to make style changes.

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
  
-    var contents = $('#copy').html();
+    $('li').css({
+        'font-size': '20px',
+        'background-color': 'yellow'
+    });
+    $('p, h1, li').css('background-color','yellow');
+
+    var contents = $('h1').html();
     alert(contents);
+ 
+
 
 });
 
