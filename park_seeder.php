11dd659b1125154beab063cba71fad798cb12742
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-12 13:13:37 -0500

    Adding data to parks_db using seeder file.

--- park_seeder.php
+++ park_seeder.php
@@ -10,6 +10,24 @@ define('DB_PASS', 'codeup');
 
 require 'db_connect.php';
 
-USE parks_db;
 
 $dbc->exec('TRUNCATE national_parks');
+
+$parks = [
+	['name' => 'Acadia', 'location' => 'Maine', 'date_established' => '1919-02-26', 'area_in_acres' => '47389.67'],
+	['name' => 'Arches', 'location' => 'Utah', 'date_established' => '1929-04-12', 'area_in_acres' => '76518.98'],
+	['name' => 'Badlands', 'location' => 'South Dakota', 'date_established' => '1978-11-10', 'area_in_acres' => '242755.94'],
+	['name' => 'Big Bend', 'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => '801163.21'],
+	['name' => 'Carlsbad Caverns', 'location' => 'New Mexico', 'date_established' => '1930-05-14', 'area_in_acres' => '46766.45'],
+	['name' => 'Glacier', 'location' => 'Montana', 'date_established' => '1910-05-11', 'area_in_acres' => '1013572.41'],
+	['name' => 'Grand Canyon', 'location' => 'Arizona', 'date_established' => '1919-02-26', 'area_in_acres' => '1217403.32'],
+	['name' => 'Joshua Tree', 'location' => 'California', 'date_established' => '1994-10-31', 'area_in_acres' => '789745.47'],
+	['name' => 'Rocky Mountain', 'location' => 'Colorado', 'date_established' => '1915-01-26', 'area_in_acres' => '265828.41'],
+	['name' => 'Shenandoah', 'location' => 'Virginia', 'date_established' => '1926-05-22', 'area_in_acres' => '199045.23']
+];
+
+foreach ($parks as $park){
+	$query = "INSERT INTO national_parks (name, location, date_established, area_in_acres) 
+	VALUES ('{$park['name']}', '{$park['location']}', '{$park['date_established']}','{$park['area_in_acres']}')";
+	$dbc->exec($query);
+}
