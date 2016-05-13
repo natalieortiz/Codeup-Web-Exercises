2f107d9bd6104cabe0a3c600a719aff89d1ae60e
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-13 18:05:36 -0500

    Limited results to 4 parks at a time and added next and previous links.

--- public/national_parks.php
+++ public/national_parks.php
@@ -11,12 +11,19 @@ define('DB_PASS', 'codeup');
 require '../db_connect.php';
 
 
+	$page = !isset($_GET['page']) ? 0 : $_GET['page'];
+
     $parks = array();
 
-    $parks = $dbc->query('SELECT * FROM national_parks')->fetchAll(PDO::FETCH_ASSOC);
-
+    if ($page == 0) {
+	    $parks = $dbc->query("SELECT * FROM national_parks LIMIT 4")->fetchAll(PDO::FETCH_ASSOC);	
+    } else if ($page >= 1) {
+    	$offset = $page * 4;
+    	$parks = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET {$offset}")->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -25,14 +32,19 @@ require '../db_connect.php';
 </head>
 <body>
 
-
     <?php foreach ($parks as $park) : ?>
- 		<h3><?= $park['name'] ?></h3>
- 		<li><?= $park['location'] ?></li>
- 		<li><?= $park['date_established'] ?></li>
- 		<li><?= $park['area_in_acres'] ?></li>
+ 		<h3><?= $park['name'] ?> National Park</h3>
+ 		<li>Located: <?= $park['location'] ?></li>
+ 		<li>Date Established: <?= $park['date_established'] ?></li>
+ 		<li>Area in Acres: <?= $park['area_in_acres'] ?></li>
  	<?php endforeach; ?>
-
+ 	<br>
+ 	<?php if ($page >= 1 ) {  ?>
+		<a href="?page=<?= $page - 1 ?>">Previous Page</a>
+	<?php } ?>
+ 	<?php if ($page >= 0 and $page < 2) { ?>
+	 	<a href="?page=<?= $page + 1 ?>">Next Page</a>
+ 	<?php } ?>
 </body>
 </html>
 
