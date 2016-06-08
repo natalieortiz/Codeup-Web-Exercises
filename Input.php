8cfbd63497602e2c141de392ebd7e937b8b3bdcc
Natalie Ortiz <ndnatalie@gmail.com>
2016-06-08 09:26:51 -0500

    Adding Input class.

--- Input.php
+++ Input.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Input
 {
     /**
@@ -25,23 +26,69 @@ class Input
         return self::has($key) ? $_REQUEST[$key] : $default;
     }
 
-    public static function getString($key)
-    {
+    public static function getString($key, $min = 1, $max = 255 )
+    {   
+
+        $stringlength = strlen($key);
+
+        if (empty(self::get($key)))
+        {
+            throw new OutOfRangeException('Value is missing');
+        }
+
         if (!is_string(self::get($key)) || is_numeric(self::get($key)))
         {
-            throw new Exception('Value must be a string.');
+            throw new DomainException('Value must be a string.');
+        } 
+
+        if ($stringlength < $min || $stringlength > $max)
+        {
+            throw new LengthException('Sting must be greater than 1 and less than 255 characters.');
+        }
+
+
+        return trim(self::get($key));
+    }
+    
+    public static function getText ($key)
+    {   
+        
+        if (empty(self::get($key)))
+        {
+            throw new OutOfRangeException('Value is missing');
+        }
+
+        if (!is_string(self::get($key)) || is_numeric(self::get($key)))
+        {
+            throw new DomainException('Value must be a string.');
         } 
 
         return trim(self::get($key));
     }
 
-    public static function getNumber($key)
+    public static function getNumber($key, $min = 1, $max = 9)
     {   
         $potentialNumber = self::get($key);
+        $stringlength = strlen(self::get($key));
+
+        if (empty(self::get($key)))
+        {
+            throw new OutOfRangeException('Value is missing');
+        }
 
         if(!is_numeric($potentialNumber))
         {
-            throw new Exception('Value must be an integer.');
+            throw new DomainException('Value must be an integer.');
+        }
+
+        if (!is_numeric($min) || !is_numeric($max)) 
+        {
+            throw new InvalidArgumentException('Min or max must be a number.');
+        }
+
+        if ($potentialNumber < $min || strlen($potentialNumber) > $max)
+        {
+            throw new RangeException('Number must be greater than 1 and less than 10,000,000.');
         }
 
         $findme = '.';
@@ -50,6 +97,20 @@ class Input
         } else {
             return floatval($potentialNumber);
         }
+
+    }
+
+    public static function getDate($key)
+    {
+        $getdate = self::get($key);
+        $date = date_create($getdate);
+        if ($date === false) {
+            throw new Exception ('Please enter in YYYY-MM-DD format.');
+        }
+
+        return $date->format('Y-m-d');
+
+
 
     }
 
